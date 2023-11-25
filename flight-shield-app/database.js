1c7bcd030ba60a0df0ae80c3c88bf3bed81dadc7
Aruna Parameswaran <aruna.parame@gmail.com>
2023-11-25 11:24:17 -0600

    tested trigger + stored procedures

--- flight-shield-app/database.js
+++ flight-shield-app/database.js
@@ -4,6 +4,7 @@ var config = {
     user: process.env.DB_USER,
     database: process.env.DB_NAME,
     password: process.env.DB_PASS,
+    multipleStatements: true
 };
 
 // Later on when running from Google Cloud, env variables will be passed in container cloud connection config
--- flight-shield-app/server.js
+++ flight-shield-app/server.js
@@ -26,7 +26,17 @@ app.post('/submit', (req, res) => {
 
 app.get('/airlines', (req, res) => {
     connection.query(
-      "SELECT * FROM `airlines`",
+      "SELECT * FROM `airlines`;",
+      (error, results, fields) => {
+        if(error) throw error;
+        res.json(results);
+      }
+    );
+});
+
+app.get('/rating/:username', (req, res) => {
+    connection.query(
+      "SELECT * FROM `user_ratings` WHERE username = ?;", req.params.username,
       (error, results, fields) => {
         if(error) throw error;
         res.json(results);
@@ -37,13 +47,104 @@ app.get('/airlines', (req, res) => {
 app.route('/airlines/:id')
   .get( (req, res, next) => {
     connection.query(
-      "SELECT * FROM `airlines` WHERE IATA_CODE = ?", req.params.id,
+      "SELECT * FROM `airlines` WHERE IATA_CODE = ?;", req.params.id,
       (error, results, fields) => {
         if(error) throw error;
         res.json(results);
       }
     );
   });
+
+app.get('/test', async (req, res) => {
+    // does not have fieldcount etc in payload
+    connection.query(
+        "CALL GetAirlineDelays();",
+        (error, results, fields) => {
+          if(error) throw error;
+          console.log(results[0][0]);
+          res.json(results[0][0]);
+        }
+      );
+});
+
+app.post('/test2', async (req, res) => {
+    console.log({
+        origin: req.body.origin,
+        destination: req.body.destination,
+        date: req.body.date,
+      });
+
+    // {"fieldCount":0,"affectedRows":0,"insertId":0,"serverStatus":2,"warningCount":0,"message":"","protocol41":true,"changedRows":0} is in index 0
+    connection.query(
+        "CALL GetTopRatedAirlines('"+ req.body.origin + "', '" + req.body.destination + "', '" + req.body.date + "', @airline_name1, @rating_val1, @airline_name2, @rating_val2); SELECT @airline_name1, @rating_val1; SELECT @airline_name2, @rating_val2;",
+        (error, results, fields) => {
+          if(error) throw error;
+
+          console.log(results);
+          res.json(results);
+        }
+      );
+});
+
+app.post('/test3', async (req, res) => {
+    // {"fieldCount":0,"affectedRows":0,"insertId":0,"serverStatus":2,"warningCount":0,"message":"","protocol41":true,"changedRows":0} is in index 1
+    connection.query(
+        "CALL GetAvgDelayWithRisk('"+ req.body.origin + "', '" + req.body.destination + "', '" + req.body.date + "');",
+        (error, results, fields) => {
+          if(error) throw error;
+
+          console.log(results);
+          res.json(results);
+        }
+      );
+});
+
+app.get('/user-rating-test', (req, res) => {
+    // res.render('/src/form.html');
+    res.sendFile(path.join(__dirname, '/src/user-rating.html'));
+  });
+
+app.post('/user-rating-trigger', async (req, res) => {
+    console.log(req.body);
+    connection.query(
+        "SELECT * FROM airlines WHERE IATA_CODE = ?; INSERT INTO user_ratings (username, IATA_CODE, rating) VALUES (?, ?, ?); SELECT * FROM airlines WHERE IATA_CODE = ?;",
+        [req.body.iata_code, req.body.username, req.body.iata_code, req.body.rating, req.body.iata_code],
+        (error, results, fields) => {
+          if(error) throw error;
+
+          console.log(results);
+          res.json(results);
+        }
+      );
+});
+
+app.post('/delete-rating', async (req, res) => {
+    console.log(req.body);
+    connection.query(
+        "SELECT * FROM airlines WHERE IATA_CODE = ?; DELETE FROM user_ratings WHERE username = ? AND IATA_CODE = ?; SELECT * FROM airlines WHERE IATA_CODE = ?;",
+        [req.body.iata_code, req.body.username, req.body.iata_code, req.body.iata_code],
+        (error, results, fields) => {
+          if(error) throw error;
+
+          console.log(results);
+          res.json(results);
+        }
+      );
+});
+
+app.post('/update-rating', async (req, res) => {
+    console.log(req.body);
+    connection.query(
+        "SELECT * FROM airlines WHERE IATA_CODE = ?; UPDATE user_ratings SET rating = ? WHERE username = ? AND IATA_CODE = ?; SELECT * FROM airlines WHERE IATA_CODE = ?;",
+        [req.body.iata_code, req.body.rating, req.body.username, req.body.iata_code, req.body.iata_code],
+        (error, results, fields) => {
+          if(error) throw error;
+
+          console.log(results);
+          res.json(results);
+        }
+      );
+});
 
 // Listen to the App Engine-specified port, or 8080 otherwise
 const PORT = parseInt(process.env.PORT) || 8080;
