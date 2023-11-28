53b26ad8ad1833646df569f942f8af8881b7e3e4
Aruna Parameswaran <aruna.parame@gmail.com>
2023-11-28 00:29:26 -0600

    Added user rating logic
    - Added ability to store username in the session
    
    - Removed redundant code + refactored
    
    - Added html + api calls for user rating

--- flight-shield-app/server.js
+++ flight-shield-app/server.js
@@ -4,26 +4,47 @@ const express = require('express');
 const connection = require('./database');
 const path = require('path');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 
 const app = express();
 
 // app.use(express.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/src/login.html'));
-});
-
-app.get('/signup', (req, res) => {
-    res.sendFile(path.join(__dirname, '/src/signup.html'));
+    if (!req.session.username) {
+        res.redirect("/login")
+    
+    } else {
+        res.redirect("/homepage")
+    }
 });
 
 app.get('/homepage', (req, res) => {
-    res.sendFile(path.join(__dirname, '/src/homepage.html'));
+    if (!req.session.username) {
+        res.redirect("/login")
+    
+    } else {
+        res.sendFile(path.join(__dirname, '/src/homepage.html'));
+    }
 });
 
-app.get('/submit', (req, res) => {
-  res.sendFile(path.join(__dirname, '/src/form.html'));
+app.get('/rating', (req, res) => {
+    if (!req.session.username) {
+        res.redirect("/login")
+    
+    } else {
+        res.sendFile(path.join(__dirname, '/src/user-rating.html'));
+    }
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/src/login.html'));
 });
 
 app.post('/login', (req, res) => {
@@ -37,6 +58,10 @@ app.post('/login', (req, res) => {
         }
         // Check if a matching user was found
         if (results.length > 0) {
+            // Save user information in session variable
+            req.session.loggedin = true;
+			req.session.username = req.body.username;
+            
             // User authenticated successfully
             return res.status(200).json({ message: 'Login successful' });
         } else {
@@ -47,7 +72,11 @@ app.post('/login', (req, res) => {
     );
 });
 
-app.post('/add-user', (req, res) => {
+app.get('/signup', (req, res) => {
+    res.sendFile(path.join(__dirname, '/src/signup.html'));
+});
+
+app.post('/signup', (req, res) => {
     console.log(req.body);
     connection.query(
       "INSERT INTO users (username, pass) VALUES (?, ?);",
@@ -59,17 +88,27 @@ app.post('/add-user', (req, res) => {
     );
 });
 
-app.post('/submit', (req, res) => {
-  console.log({
-    name: req.body.name,
-    message: req.body.message,
-  });
-  res.send('Thanks for your message!');
+app.get('/currentuser', (req, res) => {
+    if (req.session.username) {
+        return res.status(200).json({ username: req.session.username });
+    }
+    return res.status(401).json({ error: 'You must be logged in!' });
 });
 
 app.get('/airlines', (req, res) => {
     connection.query(
-      "SELECT * FROM `airlines`;",
+      "SELECT * FROM `airlines` WHERE AIRLINE <> 'nan';",
+      (error, results, fields) => {
+        if(error) throw error;
+        res.json(results);
+      }
+    );
+});
+
+app.route('/airlines/:id')
+  .get( (req, res, next) => {
+    connection.query(
+      "SELECT * FROM `airlines` WHERE IATA_CODE = ?;", req.params.id,
       (error, results, fields) => {
         if(error) throw error;
         res.json(results);
@@ -87,18 +126,7 @@ app.get('/rating/:username', (req, res) => {
     );
 });
 
-app.route('/airlines/:id')
-  .get( (req, res, next) => {
-    connection.query(
-      "SELECT * FROM `airlines` WHERE IATA_CODE = ?;", req.params.id,
-      (error, results, fields) => {
-        if(error) throw error;
-        res.json(results);
-      }
-    );
-  });
-
-app.get('/test', async (req, res) => {
+app.get('/airline-delays', async (req, res) => {
     // does not have fieldcount etc in payload
     connection.query(
         "CALL GetAirlineDelays();",
@@ -110,7 +138,7 @@ app.get('/test', async (req, res) => {
       );
 });
 
-app.post('/test2', async (req, res) => {
+app.post('/top-rated-airlines', async (req, res) => {
     console.log({
         origin: req.body.origin,
         destination: req.body.destination,
@@ -130,9 +158,6 @@ app.post('/test2', async (req, res) => {
 });
 
 app.post('/risk-analysis', async (req, res) => {
-    
-    console.log(req.body);
-    
     // {"fieldCount":0,"affectedRows":0,"insertId":0,"serverStatus":2,"warningCount":0,"message":"","protocol41":true,"changedRows":0} is in index 1
     connection.query(
         "CALL GetAvgDelayWithRisk('"+ req.body.origin + "', '" + req.body.destination + "', '" + req.body.date + "');",
@@ -145,12 +170,7 @@ app.post('/risk-analysis', async (req, res) => {
       );
 });
 
-app.get('/user-rating-test', (req, res) => {
-    // res.render('/src/form.html');
-    res.sendFile(path.join(__dirname, '/src/user-rating.html'));
-  });
-
-app.post('/user-rating-trigger', async (req, res) => {
+app.post('/add-rating', async (req, res) => {
     console.log(req.body);
     connection.query(
         "SELECT * FROM airlines WHERE IATA_CODE = ?; INSERT INTO user_ratings (username, IATA_CODE, rating) VALUES (?, ?, ?); SELECT * FROM airlines WHERE IATA_CODE = ?;",
